888d53e9a06bf73d4948d89bed51b6aac6afdc26
ASUS <kevinkhanh0701@gmail.com>
2022-04-09 02:11:13 +0700

    Add project files.

---
+++ shopxemay/Areas/Admin/Controllers/SanPhamController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using shopxemay.Models;
+using shopxemay.DAO;
+
+namespace shopxemay.Areas.Admin.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        SanPhamDAO sanphamDAO = new SanPhamDAO();
+
+        MyDBcontext db = new MyDBcontext();
+
+
+        // GET: Admin/SanPham
+        public ActionResult Index()
+        {
+           
+            return View(sanphamDAO.Getlist("Index"));
+        }
+
+        // GET: Admin/SanPham/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = sanphamDAO.getRow(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // GET: Admin/SanPham/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams, "MaLoaiSP", "TenLoaiSP");
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC");
+            return View();
+        }
+
+        // POST: Admin/SanPham/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaSP,TenSP,MoTa1,MoTa2,GiaBan,NgayCapNhat,Hinh,SolanMua,TrangThai,TonKho,SPMoi,MaLoaiSP,MaNCC")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                sanphamDAO.Insert(sanPham);
+                
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams, "MaLoaiSP", "TenLoaiSP", sanPham.MaLoaiSP);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // GET: Admin/SanPham/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = sanphamDAO.getRow(id);
+
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams, "MaLoaiSP", "TenLoaiSP", sanPham.MaLoaiSP);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // POST: Admin/SanPham/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaSP,TenSP,MoTa1,MoTa2,GiaBan,NgayCapNhat,Hinh,SolanMua,TrangThai,TonKho,SPMoi,MaLoaiSP,MaNCC")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                sanphamDAO.Update(sanPham);
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLoaiSP = new SelectList(db.LoaiSanPhams, "MaLoaiSP", "TenLoaiSP", sanPham.MaLoaiSP);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // GET: Admin/SanPham/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = sanphamDAO.getRow(id);
+
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: Admin/SanPham/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SanPham sanPham = sanphamDAO.getRow(id);
+
+            sanphamDAO.Delete(sanPham);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult TrangThai(int? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToAction("Index", "SanPham");
+            }
+             SanPham sanPham = sanphamDAO.getRow(id);
+            if (sanPham == null)
+            {
+            return RedirectToAction("Index", "SanPham");
+            }
+            sanPham.TrangThai = (sanPham.TrangThai == true) ? false : true;
+            sanphamDAO.Update(sanPham);
+            return RedirectToAction("Index", "SanPham");
+
+        }
+    }
+}
---
+++ shopxemay/DAO/SanPhamDAO.cs
@@ -0,0 +1,82 @@
+﻿using shopxemay.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace shopxemay.DAO
+{
+    public class SanPhamDAO
+    {
+
+        private MyDBcontext db = new MyDBcontext();
+        //trả về ds các mục tin
+        public List<SanPham> Getlist(string TrangThai = "All")
+        {
+            List<SanPham> list = null;
+            switch (TrangThai)
+            {
+                case "Index":
+                    {
+                        list = db.SanPhams.Where(m => m.TrangThai != true).ToList();
+                        break;
+                    }
+                case "Trash":
+                    {
+                        list = db.SanPhams.Where(m => m.TrangThai == false).ToList();
+                        break;
+
+                    }
+                default:
+                    {
+                        list = db.SanPhams.ToList();
+                        break;
+
+                    }
+            }
+            return list;
+        }
+        //trả về 1 mục tin
+        public SanPham getRow(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.SanPhams.Find(id);
+            }
+        }
+
+        //thêm Mẫu tin
+        public int Insert(SanPham row)
+        {
+            db.SanPhams.Add(row);
+            return db.SaveChanges();
+        }
+        //cập nhật Mẫu tin
+        public int Update(SanPham row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+           
+            return db.SaveChanges();
+
+           
+        }
+
+        //xóa Mẫu tin
+        public int Delete(SanPham row)
+        {
+            db.SanPhams.Remove(row);
+            return db.SaveChanges();
+
+        }
+
+    }
+}
+ 
+
+
